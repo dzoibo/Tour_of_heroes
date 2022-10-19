97fe4e165fd6ef5358178e0cb9206c08a5798c3e
Dzoibo ranites <ivandztitan@gmail.com>
2022-10-19 07:38:46 +0200

    adding navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { HeroService } from './hero.service';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule, AppRoutingModule
   ],
   providers: [MessageService,HeroService],
   bootstrap: [AppComponent]
